58fc52b4be928605cd704662c19602d56183a196
Karyna <karynanech@gmail.com>
2024-02-03 10:49:49 +0000

    edits comments and names in milestone_3.py

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,13 +1,12 @@
 #%%
 import random
 
+#define the list of words to use for the game and picka random one of them
 words=['apple', 'pear', 'nectarine', 'peach', 'pineapple']
 word=random.choice(words)
 #%%
-
-# %%
-
 def check_guess(guess):
+    #this function checks if a variable guess is present in the word defined above
     guess=guess.lower()
     if guess in word:
         print(f'Good guess! {guess} is in the word.')
@@ -15,9 +14,11 @@ def check_guess(guess):
         print(f"Sorry, {guess} is not in the word. Try again." )
 
 # %%
-def ask_for_input():
+def round_of_hangman():
+    #this function iterates iuser input until it's a single letter and then checks if it is in the word
     while True:
         guess=input('Please select a letter')
+        #check if guess is indeed a letter
         if len(guess)==1 and guess.isalpha():
             break
         else:
@@ -25,5 +26,5 @@ def ask_for_input():
     check_guess(guess)
 
 # %%
-ask_for_input()
+round_of_hangman()
 # %%
